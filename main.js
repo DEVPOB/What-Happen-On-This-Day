2177cad5a524c8cc63673e4ed9adf44ce11d140a
DEVPOB <86759611+DEVPOB@users.noreply.github.com>
2022-02-19 17:08:29 +0700

    Day&Month

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ function addZero(i) {
     if (i < 10) { i = "0" + i }
     return i;
 }
-function clock(){
+function Date_clock(){
     const d = new Date();
-    var h = addZero(d.getHours());
-    var m = addZero(d.getMinutes());
-    var s = addZero(d.getSeconds());
+    let D = d.getDate()
+    let M = d.getMonth()+1
+    let h = addZero(d.getHours());
+    let m = addZero(d.getMinutes());
+    let s = addZero(d.getSeconds());
     var am_or_pm = document.getElementById('am_or_pm');
     if (h >= 12) {
         am_or_pm = "PM"
@@ -14,12 +16,16 @@ function clock(){
     else {
         am_or_pm = "AM"
     }
-    
-    let time = h + ":" + m + ":" + s + " " + am_or_pm;
+    document.getElementById('day').innerHTML = D;
+    document.getElementById('month').innerHTML = M;
     document.getElementById('hour').innerHTML = h;
     document.getElementById('min').innerHTML = m;
     document.getElementById('sec').innerHTML = s;
+    document.getElementById('pm_am').innerHTML = am_or_pm
 
 }
-setInterval(clock,10);
+function CheckDay(){
+    console.log(Date_clock())
+}
+setInterval(Date_clock,10);
 
